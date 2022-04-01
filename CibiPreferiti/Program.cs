e74e781395922e5b0e94b28adbc04d2c94af493a
MarcoPieretti <marcopieretti50@gmail.com>
2022-04-01 13:15:22 +0200

    Prima parte completata

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿string[] cibiPreferiti = { "Carne", "Pizza", "Banane", "Lasagne", "Cannelloni", "Cioccolato Fondente", "Sushi", "Risotti", "Formaggi" };
+Console.WriteLine("La mia classifica ha: " + cibiPreferiti.Length + " elementi");
+
+Console.WriteLine("I nomi dei cibi in classifica sono:");
+for (int i = 0; i < cibiPreferiti.Length; i++)
+{
+    Console.WriteLine(cibiPreferiti[i]);
+}
+Console.WriteLine("Il mio cibo preferito in assoluto e': " + cibiPreferiti[0]);
+Console.WriteLine("il cibo tra i preferiti che prediligo di meno e': " + cibiPreferiti[8]);
